f19ea0600aa1856b436251f78bdf48959d8c0d64
Axel Acevedo <axelacevedo021@gmail.com>
2024-03-17 17:56:08 -0300

    componentes/acevedo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentPageComponent } from './student-page/student-page.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentPageComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
