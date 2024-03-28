d3e0d6ebb19af3fd26b07a7c1483e3f62faad1ad
Axel Acevedo <axelacevedo021@gmail.com>
2024-03-27 23:54:34 -0300

    lista/acevedo

--- src/app/student-page/student-page.component.ts
+++ src/app/student-page/student-page.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-student-page',
   templateUrl: './student-page.component.html',
-  styleUrl: './student-page.component.scss'
+  styleUrls: ['./student-page.component.scss']
 })
 export class StudentPageComponent {
+  estudiantes = [
+    { nombre: 'Gonzalo', nota: 70 },
+    { nombre: 'Lucas', nota: 85 },
+    { nombre: 'Anahis', nota: 60 },
+    { nombre: 'Alexis', nota: 40 },
+    { nombre: 'Alexis', nota: 55 },
+    { nombre: 'Alexis', nota: 90 },
+    { nombre: 'Mario', nota: 75 },
+    { nombre: 'Dora', nota: 30 }
+  ];
 
+  mostrarListado = false;
+
+  esAprobado(nota: number): boolean {
+    return nota >= 60;
+  }
 }
